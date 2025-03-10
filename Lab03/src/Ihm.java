fba26a87a7e8c5fa512ac0fed9d0397ae0199d93
GabrielUrbinati <152566779+GabrielUrbinati@users.noreply.github.com>
2025-03-10 20:37:56 -0300

    Update Ihm.java
    corrigido

--- Lab03/src/Ihm.java
+++ Lab03/src/Ihm.java
@@ -2,44 +2,51 @@ import java.util.List;
 import static java.lang.System.out;
 import java.util.Scanner;
 
-
-public class Ihm{
+public class Ihm {
     private GerenciadorNomes gerenciador;
     private Scanner entrada = new Scanner(System.in);
 
-    public Ihm( GerenciadorNomes g){
+    public Ihm(GerenciadorNomes g) {
         gerenciador = g;
-
     }
 
-    public void dialogar(){
+    public void dialogar() {
         boolean sair = false;
-        while(!sair){
+        while (!sair) {
             out.println("\n>> Menu Principal");
-            out.println("\n (1) Listar Nomes");
-            out.println("\n (2) Adicionar Nomes");
-            out.println("\n (3) Adicionar Nomes");
-            out.print("Escolha uma opção");
+            out.println(" (1) Listar Nomes");
+            out.println(" (2) Adicionar Nome");
+            out.println(" (3) Sair");
+            out.print("Escolha uma opção: ");
             String op = entrada.nextLine();
-            switch(op){
-                case "1" : listar(); break;
-                case "2" : adicionar(); break;
-                case "3" : sair = true; break;
-                default: out.println("Opção invalida");
+            switch (op) {
+                case "1":
+                    listar();
+                    break;
+                case "2":
+                    adicionar();
+                    break;
+                case "3":
+                    sair = true;
+                    break;
+                default:
+                    out.println("Opção inválida");
             }
-
-            
         }
-        out.println("fim do programa");
+        out.println("Fim do programa.");
     }
-    private void listar(){
-        List<String> nomes =gerenciador.obterNomes();
-        out.println("\n Lista dos nomes: ");
-        for (String nome: nomes){out.println(nome);};
+
+    private void listar() {
+        List<String> nomes = gerenciador.obterNomes();
+        out.println("\nLista de nomes:");
+        for (String nome : nomes) {
+            out.println(nome);
+        }
     }
-    private void adicionar(){
-        out.println("\n Digite o novo nome: ");
-        String novoNome= entrada.nextLine();
+
+    private void adicionar() {
+        out.println("\nDigite o novo nome:");
+        String novoNome = entrada.nextLine();
         gerenciador.adicionarNome(novoNome);
     }
 }
