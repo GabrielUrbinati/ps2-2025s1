179e37ae907a5b3c2fdda8871000fda768e93db2
GabrielUrbinati <152566779+GabrielUrbinati@users.noreply.github.com>
2025-03-10 20:38:16 -0300

    Update Main.java
    corrigido

--- Lab03/src/Main.java
+++ Lab03/src/Main.java
@@ -1,6 +1,21 @@
-public class Main{
-    public static void main (String [] args){
-        GerenciadorNomes gNomes= new GerenciadorNomesMem();
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        GerenciadorNomes gNomes;
+
+        System.out.println("Escolha o tipo de armazenamento:");
+        System.out.println("(1) Memória");
+        System.out.println("(2) Arquivo");
+        String opcao = scanner.nextLine();
+
+        if ("1".equals(opcao)) {
+            gNomes = new GerenciadorNomesMem();
+        } else {
+            gNomes = new GerenciadorNomesArq();
+        }
+
         Ihm ihm = new Ihm(gNomes);
         ihm.dialogar();
     }
