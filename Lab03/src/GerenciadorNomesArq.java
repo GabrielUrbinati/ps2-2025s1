2c6b6576f54f3ecdb481149f40346d04f50a4554
GabrielUrbinati <152566779+GabrielUrbinati@users.noreply.github.com>
2025-03-10 20:37:24 -0300

    Update GerenciadorNomesArq.java
    corrigido

--- Lab03/src/GerenciadorNomesArq.java
+++ Lab03/src/GerenciadorNomesArq.java
@@ -1,38 +1,45 @@
-import java.io.FileWriter;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GerenciadorNomesArq implements GerenciadorNomes {
 
+    private static final String ARQUIVO_NOMES = "arquivo.txt";
+    private Path filePath;
 
-    public static void obterNomes(String nomes){
-
-       Path path = Paths.get("/workspaces/ps2-2025s1/Lab03/src/arquivo.txt");
-		try {
-			byte[] bs = Files.readAllBytes(path);
-			List<String> strings = Files.readAllLines(path);
-			
-			System.out.println("Read bytes: \n"+new String(bs));
-			System.out.println("Read lines: \n"+strings);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
+    public GerenciadorNomesArq() {
+        this.filePath = Paths.get(ARQUIVO_NOMES);
+        try {
+            if (!Files.exists(filePath)) {
+                Files.createFile(filePath);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    public adicionarNomes(String nomeArq, String conteudo){
-            Path filePath = Paths.get(arquivo.txt);
-            Files.deleteIfExists(filePath);
-            Files.createFile(filePath);
-        for (String str : stringList ) {
-            Files.writeString(filePath, str + System.lineSeparator(),
-            StandardOpenOption.APPEND);
-}
-return filePath.toString();
-    }
-    
 
-    
+    @Override
+    public List<String> obterNomes() {
+        List<String> nomes = new ArrayList<>();
+        try {
+            nomes = Files.readAllLines(filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return nomes;
+    }
+
+    @Override
+    public void adicionarNome(String nome) {
+        if (nome.length() > MAX_CARACTERES_NOMES) {
+            System.out.println("Erro: Nome excede o limite de caracteres.");
+            return;
+        }
+        try {
+            Files.write(filePath, (nome + System.lineSeparator()).getBytes(), StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
